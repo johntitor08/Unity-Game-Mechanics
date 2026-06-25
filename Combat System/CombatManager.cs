abac6964d275b281c9f36474bb70fa9842e05c70
John Titor <95267320+johntitor08@users.noreply.github.com>
2026-06-25 09:04:32 +0300

    Add files via upload

--- Combat System/CombatManager.cs
+++ Combat System/CombatManager.cs
@@ -113,6 +113,19 @@ public class CombatManager : MonoBehaviour
         }
         else
         {
+            if (enemyStats != null)
+                Instance.enemyStats = enemyStats;
+
+            if (buffUIParent != null)
+                Instance.buffUIParent = buffUIParent;
+
+            if (buffUIPrefab != null)
+                Instance.buffUIPrefab = buffUIPrefab;
+
+            Instance.inCombat = false;
+            Instance.combatResolved = false;
+            Instance.currentEnemy = null;
+
             Destroy(gameObject);
         }
     }
